06f15f47a5a7483eecd279fce1105338341456a9
candraagungpurnomo <122202002820@mhs.dinus.ac.id>
2022-05-27 13:54:56 +0700

    Praktikum dan Latihan E-commerce

---
+++ minggu10/praktikum/README.md
@@ -0,0 +1,20 @@
+## 🚀 Membuat Pencarian Data Using Framework CI pada Praktikum Modul-10
+
+Materi Praktikum Pencarian Data Using Framework CI3 pada Praktikum Modul-10
+
+<hr>
+
+
+
+
+----------------------------------------->> HASIL <<-----------------------------------------
+
+- <b>Tampilan Awal Input Pencarian Data</b> 
+
+![tampilan_input_pencariandata](https://user-images.githubusercontent.com/80149680/170646364-9b1df5e8-31b1-4560-908c-53ac51c8bfb1.png)
+
+- <b>Tampilan Akhir Output Pencarian Data</b>
+
+![tampilan_output_pencariandata](https://user-images.githubusercontent.com/80149680/170646414-0052db05-a8b0-4be9-8226-fa3b485377c4.png)
+
+
---
+++ minggu10/praktikum/get_data_ci/DB/get_data_ci.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2022 at 08:47 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `get_data_ci`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nis` varchar(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nis`, `nama`, `jenis_kelamin`, `telp`, `alamat`) VALUES
+('9912', 'Maliya', 'Wanita', '0881234338', 'Jl. Kamboja'),
+('9916', 'Anissa', 'Wanita', '0881234337', 'Jl. Anggrek'),
+('9917', 'Budi', 'Pria', '0881234336', 'Jl. Anggrek'),
+('9918', 'Bambang', 'Pria', '0881234334', 'Jl. Mawar'),
+('9919', 'Andi', 'Pria', '0881234335', 'Jl. Melati');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nis`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
