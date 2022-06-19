4f8fd3a65538d575f5ccefea0dabbe52048b76d9
candraagungpurnomo <122202002820@mhs.dinus.ac.id>
2022-06-19 12:12:17 +0700

    Praktikum dan Latihan E-commerce

---
+++ minggu13/praktikum/README.md
@@ -0,0 +1,18 @@
+## 🚀 Pembuatan Kode untuk mengirim email menggunakan SMTP Gmail pada Praktikum Modul-13
+
+Materi Praktikum pengiriman email menggunakan SMTP Gmail pada Praktikum Modul-13
+
+<hr>
+
+
+
+
+----------------------------------------->> HASIL <<-----------------------------------------
+
+- <b>Tampilan Email Berhasil Dikirim</b> 
+
+![send_email_berhasil](https://user-images.githubusercontent.com/80149680/174466779-4b9b123b-1143-4408-99b0-7320f1337d48.png)
+
+- <b>Pengecekan ke-Email Tujuan-untuk mengecek apakah email yang terkirim berhasil masuk di email tujuan</b>
+
+![notif_smtp_gmail](https://user-images.githubusercontent.com/80149680/174466784-9197183f-c285-4a59-9bdb-bc931339dff9.png)
---
+++ minggu13/praktikum/ci_smtpgmail/application/controllers/Send_email.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Send_email extends CI_Controller {
+
+    /**
+     * Kirim email dengan SMTP Gmail.
+     *
+     */
+    public function index()
+    {
+      // Konfigurasi email
+        $config = [
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'protocol'  => 'smtp',
+            'smtp_host' => 'smtp.gmail.com',
+            'smtp_user' => '122202002820@mhs.dinus.ac.id',  // Email gmail
+            'smtp_pass'   => '123456789hvwvuwvdwdvwua',  // Password gmail
+            'smtp_crypto' => 'ssl',
+            'smtp_port'   => 465,
+            'crlf'    => "\r\n",
+            'newline' => "\r\n"
+        ];
+
+        // Load library email dan konfigurasinya
+        $this->load->library('email', $config);
+
+        // Email dan nama pengirim
+        $this->email->from('122202002820@mhs.dinus.ac.id', 'Candra.com');
+
+        // Email penerima
+        $this->email->to('candraagung48@gmail.com'); // Ganti dengan email tujuan
+
+        // Lampiran email, isi dengan url/path file
+        $this->email->attach('https://images.pexels.com/photos/169573/pexels-photo-169573.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+
+        // Subject email
+        $this->email->subject('Kirim Email dengan SMTP Gmail CodeIgniter | MasRud.com');
+
+        // Isi email
+        $this->email->message("Ini adalah contoh email yang dikirim menggunakan SMTP Gmail pada CodeIgniter.<br><br> Klik <strong><a href='https://masrud.com/kirim-email-codeigniter/' target='_blank' rel='noopener'>disini</a></strong> untuk melihat tutorialnya.");
+
+        // Tampilkan pesan sukses atau error
+        if ($this->email->send()) {
+            echo 'Sukses! email berhasil dikirim.';
+        } else {
+            echo 'Error! email tidak dapat dikirim.';
+        }
+    }
+}
